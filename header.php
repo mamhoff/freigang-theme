b7c682182e58ab2811177a2427f4ae0b6dcb6413
Martin Meyerhoff <mamhoff@googlemail.com>
2014-01-30 11:37:04 +0100

    initial commit

---
+++ header.php
@@ -0,0 +1,61 @@
+<!doctype html>  
+
+<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+	
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title><?php wp_title( '|', true, 'right' ); ?></title>	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				
+		<!-- media-queries.js (fallback) -->
+		<!--[if lt IE 9]>
+			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>			
+		<![endif]-->
+
+		<!-- html5.js -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<!-- wordpress head functions -->
+		<?php wp_head(); ?>
+		<!-- end of wordpress head -->
+
+		<!-- typeahead plugin - if top nav search bar enabled -->
+		<?php require_once('library/typeahead.php'); ?>
+				
+	</head>
+	
+	<body <?php body_class(); ?>>
+				
+		<header role="banner">
+	
+		</header> <!-- end header -->
+		
+		<div id="container" class="container">
+			<div class="row head-row">
+				<div class="col-md-3 fg-logo">
+					<img src="<?php bloginfo('template_directory') ?>/images/jac-logo.jpg" alt="Freigang Applications Consulting Logo"></img>
+				</div>
+				<div class="col-md-6 fg-nav-primary">
+						<?php wp_nav_menu( 
+						    	array( 
+						    		'menu' => 'main-menu', /* menu name */
+						    		'menu_class' => 'fg-main-menu',
+						    		'theme_location' => 'main_nav', /* where in the theme it's assigned */
+						    		'container' => 'fg-main-menu', /* container class */
+						    		// 'depth' => '2',  suppress lower levels for now 
+						    	)
+						    ); ?>
+				</div>
+				<div class="col-md-3 fg-social">
+				</div>
+			</div>	
+		
